17dac961d5f74c9561d494082cc09350ce3df625
Saphal47 <saphalkumar13@gmail.com>
2023-05-16 18:11:42 +0530

    Final changes

--- LRU1.cpp
+++ LRU1.cpp
@@ -31,15 +31,15 @@ class LRUCache{
         //2 cases
         if(mp.count(key) != 0){
             //exists so replace the value in list
-            auto it = m[key] //address
+            auto it = mp[key]; //address
             it->val = val;
         }
         else{
             //not present
             //check cache full or not, yes remove LRU item
-            if(l.size ==maxSize){
+            if(l.size() ==maxSize){
                 Node last = l.back();
-                m.erase(last.key); //remove from the hashmap
+                mp.erase(last.key); //remove from the hashmap
                 l.pop_back();//remove the last ele from list
             }
 
@@ -52,12 +52,31 @@ class LRUCache{
     }
 
     //getValue
-    int getValue(string key){
+    int* getValue(string key){
+
+        //check if key is present 
+        if(mp.count(key) != 0){
+            //get address from the hashmap
+            auto it = mp[key];
+            //Now this node should come to top as used most recently
+
+            int value = it->val;
+            l.push_front(*it);
+            l.erase(it);
+
+            //update addresss
+            mp[key] = l.begin();
+            return &l.begin()->val;
+        }
+
+        //in Cache key may be absent so return NULL 
+        return NULL;
 
     }
 
     string getMostRecentKey(){
-        
+        //head of LL
+        return l.front().key;
     }
 
 };
@@ -65,5 +84,36 @@ class LRUCache{
 
 int main(){
     
+    LRUCache lru(3);
+    lru.insert("mango",10);
+    lru.insert("apple",30);
+    lru.insert("guava",70);
+
+    cout<<lru.getMostRecentKey()<<endl;
+
+    lru.insert("mango",40);
+    cout<<lru.getMostRecentKey()<<endl;
+
+    int *order = lru.getValue("mango");
+    if(order != NULL){
+        cout<<"Mango: "<<*order<<endl;
+    }
+
+    lru.insert("banana",20);
+    cout<<lru.getMostRecentKey()<<endl;
+
+    if(lru.getValue("apple")==NULL){
+        cout<<"Apple DNE";
+    }
+    if(lru.getValue("guava")==NULL){
+        cout<<"Guava DNE";
+    }
+    if(lru.getValue("banana")==NULL){
+        cout<<"Banana DNE";
+    }
+    if(lru.getValue("mango")==NULL){
+        cout<<"Mango DNE";
+    }
+
     return 0;
 }
